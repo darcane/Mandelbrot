7e29a7dcfe909049b77fecb2d8c879e6aaa306f7
Erdem Darcan <darcane@itu.edu.tr>
2018-11-22 12:12:51 +0300

    coloring tryings.
    HSVtoRGB method explicited.

--- sketch.js
+++ sketch.js
@@ -1,14 +1,19 @@
 let minX, minY, maxX, maxY;
-let iterate = 1000;
+let iterate = 500;
 let mX, mY;
 let frDiv;
 
 function setup() {
     createCanvas(1200, 800);
-    frDiv = createDiv('');
-    mandelbrot(-2,-1,1,1);
-
     // createCanvas(1000,1000);
+    frDiv = createDiv('');
+    reset();
+    let btn = createButton("Reset");
+    btn.mousePressed(reset);
+}
+
+function reset() {
+    mandelbrot(-2, -1, 1, 1);
     // mandelbrot(-2,-2,2,2);
 }
 
@@ -21,12 +26,12 @@ function mandelbrot(xF, yF, xL, yL) {
     loadPixels();
     for (let x = 0; x < width; x++) {
         for (let y = 0; y < height; y++) {
-            let a,b;
-            if(minX < maxX) a = map(x, 0, width, minX, maxX);
-            else a = map(x, 0, width, maxX,minX);
-            if(minY < maxY) b = map(y, 0,height, minY, maxY);
-            else b = map(y, 0,height, maxY,minY);
-            let n = 0;
+            let a, b;
+            if (minX < maxX) a = map(x, 0, width, minX, maxX);
+            else a = map(x, 0, width, maxX, minX);
+            if (minY < maxY) b = map(y, 0, height, minY, maxY);
+            else b = map(y, 0, height, maxY, minY);
+            let n;
 
             let ca = a,
                 cb = b;
@@ -37,61 +42,32 @@ function mandelbrot(xF, yF, xL, yL) {
                 a = aa + ca;
                 b = bb + cb;
 
-                if (a + b > 40) {
+                if (aa * aa + bb * bb > 1000) {
                     break;
                 }
             }
 
-            let h =  n+ 1 - Math.log(Math.log(sqrt(a*a + b*b)))/ Math.log(2);
-            h = h/iterate;
-            h = map(h,0,1,0,360); //??
+            // let h = n + 1 - Math.log(Math.log(sqrt(a * a + b * b))) / Math.log(2);
+            // h = h / iterate;
+            // h = map(h, 0, 1, 0, 360); //??
             // console.log(h);
-            let s = 0.6;
-            let v = 1;
-            let c = v*s;
-            let xVal = c*(1-abs((h/60)%2-1));
-            let m = v-c;
-
-            let r1,g1,b1;
-            if(0<=h && h<60){
-                r1=c;g1=xVal;b1=0;
-            }else if(h<120){
-                r1=xVal;g1=c;b1=0;
-            }else if(h<180){
-                r1=0;g1=c;b1=xVal;
-            }else if(h<240){
-                r1=0;g1=xVal;b1=c;
-            }else if(h<300){
-                r1=xVal;g1=0;b1=c;
-            }else{
-                r1=c;g1=0;b1=xVal;
-            }
-
-            let bright = map(n, 0, iterate, 0, 255);
-            //bright = map(sqrt(bright), 0, 1, 0, 255);
-            if (n === iterate) bright = 0;
-
-            let pix = (x + y * width) * 4;
-
-            pixels[pix + 0] = (r1+m)*255;
-            pixels[pix + 1] = (g1+m)*255;
-            pixels[pix + 2] = (b1+m)*255;
-
-
-            // pixels[pix + 0] = bright;
-            // pixels[pix + 1] = bright;
-            // pixels[pix + 2] = bright;
-            pixels[pix + 3] = 255;
+            let h = n - Math.log2(Math.log(sqrt(a * a + b * b)) / Math.log(1000));
+            h = (h - n) * 360;
+            // console.log(h);
+            colorPixel(x, y, h, 0.6, 1);
         }
     }
     updatePixels();
 
 
-    frDiv.html('X wide : ['+(abs(minX-maxX)+'] </br>Y wide: ['+abs(minY-maxY))+']');
+    frDiv.style("color:white");
+    frDiv.html('X wide : [' + (abs(minX - maxX) + '] </br>Y wide: [' + abs(minY - maxY)) + ']');
 }
 
 let prevMouseX, prevMouseY;
+
 function mousePressed() {
+    if (mouseX > width || mouseY > height) return;
     prevMouseX = mouseX;
     prevMouseY = mouseY;
     mX = map(mouseX, 0, width, minX, maxX);
@@ -100,6 +76,7 @@ function mousePressed() {
 }
 
 function mouseReleased() {
+    if (mouseX > width || mouseY > height) return;
     let rX = map(mouseX, 0, width, minX, maxX);
     let rY = map(mouseY, 0, height, minY, maxY);
     //zoom(mX,mY,mouseX,mouseY);
@@ -107,9 +84,54 @@ function mouseReleased() {
     console.log("Release at :" + rX + " " + rY);
 }
 
-function mouseDragged(){
+function mouseDragged() {
+    if (mouseX > width || mouseY > height) return;
     strokeWeight(1);
     stroke(255);
     noFill();
     rect(prevMouseX, prevMouseY, mouseX - prevMouseX, mouseY - prevMouseY);
+}
+
+// hue: [0,360], sat: [0,1], val: [0,1]
+function colorPixel(pixelX, pixelY, hue, sat, val) {
+    let C = val * sat;
+    let X = C * (1 - abs((hue / 60) % 2 - 1));
+    let m = val - C;
+
+    let r1, g1, b1;
+    if (0 <= hue && hue < 60) {
+        r1 = C;
+        g1 = X;
+        b1 = 0;
+    } else if (hue < 120) {
+        r1 = X;
+        g1 = C;
+        b1 = 0;
+    } else if (hue < 180) {
+        r1 = 0;
+        g1 = C;
+        b1 = X;
+    } else if (hue < 240) {
+        r1 = 0;
+        g1 = X;
+        b1 = C;
+    } else if (hue < 300) {
+        r1 = X;
+        g1 = 0;
+        b1 = C;
+    } else if (hue < 360) {
+        r1 = C;
+        g1 = 0;
+        b1 = X;
+    } else {
+        r1 = -m;
+        g1 = -m;
+        b1 = -m;
+    }
+
+    let pix = (pixelX + pixelY * width) * 4;
+    pixels[pix + 0] = (r1 + m) * 255;
+    pixels[pix + 1] = (g1 + m) * 255;
+    pixels[pix + 2] = (b1 + m) * 255;
+    pixels[pix + 3] = 255;
 }
