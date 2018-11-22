198ca3a7cd3364e769ea40e956d33514d68025b9
Erdem Darcan <darcane@itu.edu.tr>
2018-11-22 11:13:44 +0300

    coloring alghorithms
    tried mandelbrot coloring but no luck. injected hsv to rgb color conversion

--- sketch.js
+++ sketch.js
@@ -1,12 +1,15 @@
 let minX, minY, maxX, maxY;
-let iterate = 250;
+let iterate = 1000;
 let mX, mY;
-// let frDiv;
+let frDiv;
 
 function setup() {
-    createCanvas(2000, 2000);
-    // frDiv = createDiv('');
-    mandelbrot(-2, -2, 2, 2);
+    createCanvas(1200, 800);
+    frDiv = createDiv('');
+    mandelbrot(-2,-1,1,1);
+
+    // createCanvas(1000,1000);
+    // mandelbrot(-2,-2,2,2);
 }
 
 function mandelbrot(xF, yF, xL, yL) {
@@ -34,23 +37,57 @@ function mandelbrot(xF, yF, xL, yL) {
                 a = aa + ca;
                 b = bb + cb;
 
-                if (a + b > 8) {
+                if (a + b > 40) {
                     break;
                 }
             }
+
+            let h =  n+ 1 - Math.log(Math.log(sqrt(a*a + b*b)))/ Math.log(2);
+            h = h/iterate;
+            h = map(h,0,1,0,360); //??
+            // console.log(h);
+            let s = 0.6;
+            let v = 1;
+            let c = v*s;
+            let xVal = c*(1-abs((h/60)%2-1));
+            let m = v-c;
+
+            let r1,g1,b1;
+            if(0<=h && h<60){
+                r1=c;g1=xVal;b1=0;
+            }else if(h<120){
+                r1=xVal;g1=c;b1=0;
+            }else if(h<180){
+                r1=0;g1=c;b1=xVal;
+            }else if(h<240){
+                r1=0;g1=xVal;b1=c;
+            }else if(h<300){
+                r1=xVal;g1=0;b1=c;
+            }else{
+                r1=c;g1=0;b1=xVal;
+            }
+
             let bright = map(n, 0, iterate, 0, 255);
             //bright = map(sqrt(bright), 0, 1, 0, 255);
             if (n === iterate) bright = 0;
 
             let pix = (x + y * width) * 4;
-            pixels[pix + 0] = -bright;
-            pixels[pix + 1] = sqrt(bright);
-            pixels[pix + 2] = bright;
+
+            pixels[pix + 0] = (r1+m)*255;
+            pixels[pix + 1] = (g1+m)*255;
+            pixels[pix + 2] = (b1+m)*255;
+
+
+            // pixels[pix + 0] = bright;
+            // pixels[pix + 1] = bright;
+            // pixels[pix + 2] = bright;
             pixels[pix + 3] = 255;
         }
     }
     updatePixels();
-    // frDiv.html('X : ['+minX+','+maxX+'] </br>Y: ['+minY+','+maxY+']');
+
+
+    frDiv.html('X wide : ['+(abs(minX-maxX)+'] </br>Y wide: ['+abs(minY-maxY))+']');
 }
 
 let prevMouseX, prevMouseY;
